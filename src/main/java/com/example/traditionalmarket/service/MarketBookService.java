23702fec309120b076babc871f3fd374993a1ec3
aaahyunseo <hs030608@naver.com>
2025-06-21 00:23:11 +0900

    feature : MarketBookService 생성 - #7

---
+++ src/main/java/com/example/traditionalmarket/service/MarketBookService.java
@@ -0,0 +1,32 @@
+package com.example.traditionalmarket.service;
+
+import com.example.traditionalmarket.dto.response.marketbook.MarketBookResponseData;
+import com.example.traditionalmarket.entity.Market;
+import com.example.traditionalmarket.entity.MarketBook;
+import com.example.traditionalmarket.entity.User;
+import com.example.traditionalmarket.entity.VisitedMarket;
+import com.example.traditionalmarket.repository.MarketRepository;
+import com.example.traditionalmarket.repository.VisitedMarketRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MarketBookService {
+
+    private final MarketRepository marketRepository;
+    private final VisitedMarketRepository visitedMarketRepository;
+
+    public MarketBookResponseData getMarketBook(User user) {
+        MarketBook marketBook = user.getMarketBook();
+
+        List<Market> allMarkets = marketRepository.findAll();
+
+        List<VisitedMarket> visitedMarkets = visitedMarketRepository.findByMarketBook(marketBook);
+
+        return MarketBookResponseData.of(allMarkets, visitedMarkets);
+    }
+}
+
