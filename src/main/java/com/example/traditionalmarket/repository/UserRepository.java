4a64b4f61b9b7fb45774581985414973092290aa
aaahyunseo <hs030608@naver.com>
2025-06-20 14:13:15 +0900

    feature : UserRepository 생성 - #3

---
+++ src/main/java/com/example/traditionalmarket/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.traditionalmarket.repository;
+
+import com.example.traditionalmarket.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
