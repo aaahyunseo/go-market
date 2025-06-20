1a038cc6eafe51b8c6af8c9193e80fb5caeb4d40
aaahyunseo <hs030608@naver.com>
2025-06-21 00:24:06 +0900

    feature : VisitedMarketRepository findByMarketBook 추가 - #7

--- src/main/java/com/example/traditionalmarket/repository/VisitedMarketRepository.java
+++ src/main/java/com/example/traditionalmarket/repository/VisitedMarketRepository.java
@@ -1,9 +1,12 @@
 package com.example.traditionalmarket.repository;
 
+import com.example.traditionalmarket.entity.MarketBook;
 import com.example.traditionalmarket.entity.VisitedMarket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface VisitedMarketRepository extends JpaRepository<VisitedMarket, UUID> {
+    List<VisitedMarket> findByMarketBook(MarketBook marketBook);
 }
