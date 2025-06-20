7dd8e4db343667b9254b8c962483402c3f031ca7
aaahyunseo <hs030608@naver.com>
2025-06-21 02:10:43 +0900

    feature : MarketController 생성 - #7

---
+++ src/main/java/com/example/traditionalmarket/controller/MarketController.java
@@ -0,0 +1,24 @@
+package com.example.traditionalmarket.controller;
+
+import com.example.traditionalmarket.dto.ResponseDto;
+import com.example.traditionalmarket.dto.response.market.MarketResponseData;
+import com.example.traditionalmarket.service.MarketService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/markets")
+public class MarketController {
+    private final MarketService marketService;
+
+    @GetMapping
+    public ResponseEntity<ResponseDto<MarketResponseData>> getUserInfo() {
+        MarketResponseData markets = marketService.getAllMarkets();
+        return new ResponseEntity<>(ResponseDto.res(HttpStatus.OK, "전통시장 리스트 조회 완료", markets), HttpStatus.OK);
+    }
+}
