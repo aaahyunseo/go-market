71a77bea0bff00d1c63ee1d45a9f4db81018b126
aaahyunseo <hs030608@naver.com>
2025-06-20 14:12:35 +0900

    feature : User 엔티티 생성 - #3

---
+++ src/main/java/com/example/traditionalmarket/entity/User.java
@@ -0,0 +1,35 @@
+package com.example.traditionalmarket.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+@Table(name = "users")
+public class User extends BaseEntity {
+    // 유저 아이디
+    @Column(nullable = false)
+    private String userId;
+
+    // 유저 비밀번호
+    @Column(nullable = false)
+    private String password;
+
+    // 유저 이름
+    @Column(nullable = false)
+    private String name;
+
+    // 유저 시장도감
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private MarketBook marketBook;
+
+    // 유저가 추천 받은 코스 목록
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Course> courses;
+}
