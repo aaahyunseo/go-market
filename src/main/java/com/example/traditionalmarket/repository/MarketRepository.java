8d89ab56a03992214d677a93cd276c8b921e4dc1
aaahyunseo <hs030608@naver.com>
2025-06-20 14:13:32 +0900

    feature : MarketRepository 생성 - #3

---
+++ src/main/java/com/example/traditionalmarket/repository/MarketRepository.java
@@ -0,0 +1,9 @@
+package com.example.traditionalmarket.repository;
+
+import com.example.traditionalmarket.entity.Market;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface MarketRepository extends JpaRepository<Market, UUID> {
+}
