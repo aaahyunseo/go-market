f6f8011b47c0de73b50a33c1423a1e77aa9a4644
aaahyunseo <hs030608@naver.com>
2025-06-20 12:59:33 +0900

    init : dev initialization - #1

---
+++ src/main/java/com/example/traditionalmarket/TraditionalMarketApplication.java
@@ -0,0 +1,15 @@
+package com.example.traditionalmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class TraditionalMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraditionalMarketApplication.class, args);
+    }
+
+}
