0db7f8e06920f5b48980389bb1e3fade96f05a6e
aaahyunseo <hs030608@naver.com>
2025-06-24 15:53:37 +0900

    feature : Swagger 상세 정보 추가 - #20

--- src/main/java/com/example/traditionalmarket/controller/MarketController.java
+++ src/main/java/com/example/traditionalmarket/controller/MarketController.java
@@ -3,6 +3,7 @@ package com.example.traditionalmarket.controller;
 import com.example.traditionalmarket.dto.ResponseDto;
 import com.example.traditionalmarket.dto.response.market.MarketResponseData;
 import com.example.traditionalmarket.service.MarketService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,12 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class MarketController {
     private final MarketService marketService;
 
+    @Operation(summary = "전체 전통시장 리스트 조회", description = "모든 지역에 대한 전통시장 목록이 조회됩니다.")
     @GetMapping("/all")
     public ResponseEntity<ResponseDto<MarketResponseData>> getAllMarkets() {
         MarketResponseData markets = marketService.getAllMarkets();
         return new ResponseEntity<>(ResponseDto.res(HttpStatus.OK, "전체 전통시장 리스트 조회 완료", markets), HttpStatus.OK);
     }
 
+    @Operation(summary = "지역별 전통시장 리스트 조회", description = "유저가 선택한 지역별 전통시장 목록이 조회됩니다.")
     @GetMapping
     public ResponseEntity<ResponseDto<MarketResponseData>> getMarketsByRegion(@RequestParam("region") String region) {
         MarketResponseData markets = marketService.getRegionalMarkets(region);
--- src/main/java/com/example/traditionalmarket/controller/VisitedMarketController.java
+++ src/main/java/com/example/traditionalmarket/controller/VisitedMarketController.java
@@ -5,6 +5,7 @@ import com.example.traditionalmarket.dto.ResponseDto;
 import com.example.traditionalmarket.dto.request.visitMarket.VisitedMarketDto;
 import com.example.traditionalmarket.entity.User;
 import com.example.traditionalmarket.service.VisitedMarketService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class VisitedMarketController {
     private final VisitedMarketService visitedMarketService;
 
+    @Operation(summary = "방문한 시장 등록", description = "GPS의 x,y좌표를 시장의 위치와 비교해 방문한 시장을 등록합니다.")
     @PostMapping
     public ResponseEntity<ResponseDto<Void>> addVisitedMarket(@AuthenticatedUser User user, @RequestBody @Valid VisitedMarketDto dto) {
         visitedMarketService.addVisitedMarket(user, dto);
