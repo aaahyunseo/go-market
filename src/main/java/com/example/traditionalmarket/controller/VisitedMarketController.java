f3a8384db0b4ed20944d0517623ba18afd6f5d6c
aaahyunseo <hs030608@naver.com>
2025-06-28 00:46:25 +0900

    hotfix: swagger 오류 수정

--- src/main/java/com/example/traditionalmarket/controller/VisitedMarketController.java
+++ src/main/java/com/example/traditionalmarket/controller/VisitedMarketController.java
@@ -6,16 +6,17 @@ import com.example.traditionalmarket.dto.request.visitMarket.VisitedMarketDto;
 import com.example.traditionalmarket.entity.User;
 import com.example.traditionalmarket.service.VisitedMarketService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/visited-markets")
 public class VisitedMarketController {
@@ -23,7 +24,7 @@ public class VisitedMarketController {
 
     @Operation(summary = "방문한 시장 등록", description = "GPS의 x,y좌표를 시장의 위치와 비교해 방문한 시장을 등록합니다.")
     @PostMapping
-    public ResponseEntity<ResponseDto<Void>> addVisitedMarket(@AuthenticatedUser User user, @RequestBody @Valid VisitedMarketDto dto) {
+    public ResponseEntity<ResponseDto<Void>> addVisitedMarket(@Parameter(hidden = true) @AuthenticatedUser User user, @RequestBody @Valid VisitedMarketDto dto) {
         visitedMarketService.addVisitedMarket(user, dto);
         return new ResponseEntity<>(ResponseDto.res(HttpStatus.OK, "방문한 시장 등록 완료"), HttpStatus.CREATED);
     }
