91310c3400d4492ea198b19b994f386babf23076
aaahyunseo <hs030608@naver.com>
2025-07-01 15:08:40 +0900

    feature : ChatAnswerDto 생성 - #22

---
+++ src/main/java/com/example/traditionalmarket/dto/response/Chat/ChatAnswerDto.java
@@ -0,0 +1,12 @@
+package com.example.traditionalmarket.dto.response.Chat;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatAnswerDto {
+    private String answer;
+}
