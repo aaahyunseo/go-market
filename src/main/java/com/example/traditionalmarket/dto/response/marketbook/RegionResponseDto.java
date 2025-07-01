2aec23122628b65e6065fa7cf6d2f6ca9d5b3c83
aaahyunseo <hs030608@naver.com>
2025-07-01 15:35:33 +0900

    feature : RegionResponseDto 생성 - #35

---
+++ src/main/java/com/example/traditionalmarket/dto/response/marketbook/RegionResponseDto.java
@@ -0,0 +1,10 @@
+package com.example.traditionalmarket.dto.response.marketbook;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class RegionResponseDto {
+    private String region;
+}
