87b2d9bff486485b9cf261677d3ace5964425758
aaahyunseo <hs030608@naver.com>
2025-07-02 15:33:45 +0900

    fix : 지역별 시장 도감 조회 오류 수정 - #41

--- src/main/java/com/example/traditionalmarket/service/MarketBookService.java
+++ src/main/java/com/example/traditionalmarket/service/MarketBookService.java
@@ -10,6 +10,7 @@ import com.example.traditionalmarket.exception.NotFoundException;
 import com.example.traditionalmarket.exception.errorcode.ErrorCode;
 import com.example.traditionalmarket.repository.MarketRepository;
 import com.example.traditionalmarket.repository.VisitedMarketRepository;
+import com.example.traditionalmarket.utils.RegionUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -67,31 +68,9 @@ public class MarketBookService {
         Market market = marketRepository.findByName(marketBookRequestDto.getMarketName())
                 .orElseThrow(() -> new NotFoundException(ErrorCode.MARKET_NAME_NOT_FOUND));
 
-        String region = extractRegionFromAddress(market.getAddress());
+        String region = RegionUtils.extractRegionFromAddress(market.getAddress());
 
         return new RegionResponseDto(region);
-    }
-
-    private String extractRegionFromAddress(String address) {
-        if (address == null) return "기타";
-        if (address.startsWith("서울")) return "서울";
-        if (address.startsWith("경기")) return "경기";
-        if (address.startsWith("부산")) return "부산";
-        if (address.startsWith("대구")) return "대구";
-        if (address.startsWith("광주")) return "광주";
-        if (address.startsWith("대전")) return "대전";
-        if (address.startsWith("울산")) return "울산";
-        if (address.startsWith("세종")) return "세종";
-        if (address.startsWith("인천")) return "인천";
-        if (address.startsWith("강원")) return "강원";
-        if (address.startsWith("충북")) return "충청북도";
-        if (address.startsWith("충남")) return "충청남도";
-        if (address.startsWith("전북")) return "전라북도";
-        if (address.startsWith("전남")) return "전라남도";
-        if (address.startsWith("경북")) return "경상북도";
-        if (address.startsWith("경남")) return "경상남도";
-        if (address.startsWith("제주")) return "제주";
-        return "기타";
     }
 
 }
