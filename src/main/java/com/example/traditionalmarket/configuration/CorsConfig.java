0c20dcfef1a1de571df02810e961a40e43bcae20
aaahyunseo <hs030608@naver.com>
2025-07-03 01:13:08 +0900

    fix : cookie 모바일 오류 해결 6

--- src/main/java/com/example/traditionalmarket/configuration/CorsConfig.java
+++ src/main/java/com/example/traditionalmarket/configuration/CorsConfig.java
@@ -1,33 +1,40 @@
 package com.example.traditionalmarket.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
+
     private final long MAX_AGE_SECS = 3600L;
-    @Value("#{'${client.host}'.split(',')}")
-    private List<String> clientHosts;
-    private final List<String> allowedMethods = List.of(
+
+    private final List<String> clientHosts = Arrays.asList(
+            "https://market-gogo.netlify.app",
+            "https://market-gogo.com",
+            "http://localhost:3000",
+            "https://api.market-gogo.com"
+    );
+
+    private final List<String> allowedMethods = Arrays.asList(
             "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE"
     );
 
     @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         clientHosts.forEach(config::addAllowedOrigin);
-        allowedMethods.forEach(config::addAllowedMethod);
-        config.setAllowCredentials(true);
-        config.addAllowedHeader("*");
-        config.setMaxAge(MAX_AGE_SECS);
+
+        config.setAllowCredentials(true);                // 쿠키 포함 허용
+        config.setAllowedHeaders(List.of("*"));          // 모든 헤더 허용
+        config.setAllowedMethods(allowedMethods);        // 필요한 메소드만 허용
+        config.setMaxAge(MAX_AGE_SECS);                  // Preflight 캐시 시간
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
