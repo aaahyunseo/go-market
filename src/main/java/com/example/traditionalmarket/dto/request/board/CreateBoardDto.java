f44366e6d1716c95cd447c623294d895766440eb
aaahyunseo <hs030608@naver.com>
2025-07-10 00:30:16 +0900

    feature : Board 관련 Request Dto 추가 - #53

---
+++ src/main/java/com/example/traditionalmarket/dto/request/board/CreateBoardDto.java
@@ -0,0 +1,18 @@
+package com.example.traditionalmarket.dto.request.board;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateBoardDto {
+    @NotBlank(message = "제목을 입력해주세요.")
+    private String title;
+    @NotBlank(message = "내용을 입력해주세요.")
+    private String content;
+}
