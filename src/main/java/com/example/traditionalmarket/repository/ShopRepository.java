49f2e973b4d647ff935ec9cd13c57f7f7db5c669
aaahyunseo <hs030608@naver.com>
2025-07-10 00:27:29 +0900

    feature : 사용하지 않는 엔티티 삭제 - #53

--- src/main/java/com/example/traditionalmarket/repository/ShopRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.traditionalmarket.repository;
-
-import com.example.traditionalmarket.entity.Shop;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ShopRepository extends JpaRepository<Shop, UUID> {
-}
