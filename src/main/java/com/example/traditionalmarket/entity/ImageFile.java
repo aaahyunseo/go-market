0176ebb31acffa33b066b67f142ecd30f56b95d7
aaahyunseo <hs030608@naver.com>
2025-07-10 16:21:31 +0900

    feature : ImageFile Entity, Repository, Service 생성 - #53

---
+++ src/main/java/com/example/traditionalmarket/entity/ImageFile.java
@@ -0,0 +1,26 @@
+package com.example.traditionalmarket.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "image_files")
+public class ImageFile extends BaseEntity {
+    @Column
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+}
