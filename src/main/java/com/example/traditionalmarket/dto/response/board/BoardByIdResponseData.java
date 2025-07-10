19d070cc527d4024ee798d78efdd5a117630fb67
aaahyunseo <hs030608@naver.com>
2025-07-10 16:23:01 +0900

    feature : Board response dto 이미지 추가 - #53

--- src/main/java/com/example/traditionalmarket/dto/response/board/BoardByIdResponseData.java
+++ src/main/java/com/example/traditionalmarket/dto/response/board/BoardByIdResponseData.java
@@ -1,5 +1,6 @@
 package com.example.traditionalmarket.dto.response.board;
 
+import com.example.traditionalmarket.dto.response.image.ImageData;
 import com.example.traditionalmarket.entity.Board;
 import com.example.traditionalmarket.entity.Reaction;
 import com.example.traditionalmarket.entity.ReactionType;
@@ -8,6 +9,7 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -20,12 +22,17 @@ public class BoardByIdResponseData {
     private String content;
     private String createdAt;
     private String author;
+    private List<ImageData> imageDataList;
     private int likeCount;
     private int dislikeCount;
     private boolean liked;
     private boolean disliked;
 
     public static BoardByIdResponseData of(Board board, Reaction userReaction) {
+        List<ImageData> images = board.getImageFiles().stream()
+                .map(imageFile -> new ImageData(imageFile.getId(), imageFile.getImageUrl()))
+                .toList();
+
         return BoardByIdResponseData.builder()
                 .marketName(board.getMarket().getName())
                 .boardId(board.getId())
@@ -33,6 +40,7 @@ public class BoardByIdResponseData {
                 .content(board.getContent())
                 .createdAt(board.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
                 .author(board.getUser().getUserId())
+                .imageDataList(images)
                 .likeCount(board.getLikeCount())
                 .dislikeCount(board.getDisLikeCount())
                 .liked(userReaction != null && userReaction.getReactionType() == ReactionType.LIKE)
